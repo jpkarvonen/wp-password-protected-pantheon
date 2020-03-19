ebfb5f313e6c45e0cadd75a7ad8b1beb4684a2f6
John Karvonen <karvonen@Johns-MacBook-Pro.local>
2020-03-19 16:54:05 -0400

    make variable consistent

--- wp-password-protect-pantheon.php
+++ wp-password-protect-pantheon.php
@@ -6,9 +6,9 @@
  */
 
 
-function wp_password_protected_pantheon_add_styx_prefix($pw_protect_cookie_name)
+function wp_password_protected_pantheon_add_styx_prefix($pw_protected_cookie_name)
   {
-      return 'STYXKEY_' . $pw_protect_cookie_name;
+      return 'STYXKEY_' . $pw_protected_cookie_name;
   }
 
 add_filter('password_protected_set_cookie', 'wp_password_protected_pantheon_add_styx_prefix');
